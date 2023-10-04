dbd3e1cc265950bfe9a48e7111b1e65756abc063
Enailly <129402483+Enailly@users.noreply.github.com>
2023-10-04 11:33:23 -0300

    Add files via upload

---
+++ python/primeira.py
@@ -0,0 +1,5 @@
+none = input("Digite seu nome: ")
+none = int(input("Digite sua idade: "))
+none = float(input("Digite seu peso: "))
+
+
---
+++ python/terceiro.py
@@ -0,0 +1,9 @@
+idade = int(input("Informe sua idade: "))
+
+if idade >= 18:
+    print("Pode curtir a balada!")
+
+else:
+    print("Você foi barrado!")
+
+    
